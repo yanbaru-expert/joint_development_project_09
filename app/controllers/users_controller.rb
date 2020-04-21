f6a8f81a12fc24a1486d60b03609cbeb42b4f4e7
sena555 <sayaka095@gmaii.com>
2020-04-21 23:12:04 +0900

    4.一覧表示機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
+
   def create
     User.create(user_params)
   end
