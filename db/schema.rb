70fc124b7dcab578fa348df3ffaab8f77d8417f9
kurumadaisuke <kuruma0909@gmail.com>
2020-04-21 21:31:51 +0900

    nee create訂正完了

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 2020_04_21_095502) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
